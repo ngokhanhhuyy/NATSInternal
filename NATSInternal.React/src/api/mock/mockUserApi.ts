5cab459af7ff63d2c22aface2b9671e16422932e
Ngô Khánh Huy <93859822+ngokhanhhuyy@users.noreply.github.com>
2025-10-23 21:00:42 +0900

    feat: enhance user API to include role and permission mapping in user list

--- NATSInternal.React/src/api/mock/mockUserApi.ts
+++ NATSInternal.React/src/api/mock/mockUserApi.ts
@@ -1,16 +1,43 @@
-import { useMockDatabase } from "./mockDatabase";
+import { useMockDatabase, type User, type Role } from "./mockDatabase";
 import type { UserApi } from "../userApi";
 
 const mockDatabase = useMockDatabase();
 
 const userApi: UserApi = {
   getListAsync: async (requestDto) => {
-    const result
-    const query = () => {
-      switch (requestDto.sortByFieldName) {
-        case "CreatedDateTime":
-          
+    const users = mockDatabase.users.map(user => {
+      const roleIds = mockDatabase.userRoles
+        .filter(userRole => userRole.userId == user.id)
+        .map(userRole => userRole.roleId);
+        
+      return {
+        ...user,
+        roles: mockDatabase.roles
+          .filter(role => roleIds.includes(role.id))
+          .map(role => ({
+            ...role,
+            permissionNames: mockDatabase.permissions.filter(permission => permission.roleId === role.id)
+          }))
       }
-    }
+    });
+    const filteredUsers: User[]
+
+    // const filteredUsers = mockDatabase.users.filter(user => {
+    //   let shouldSelect = false;
+    //   if (requestDto.searchContent && requestDto.searchContent.toLowerCase().includes(user.userName.toLowerCase())) {
+    //     shouldSelect = true;
+    //   }
+
+    //   if (requestDto.roleId && user.r)
+    // });
   } 
 };
+
+function doesUserMatchSearchContent(user: User, searchContent: string): boolean {
+  const lowerCaseSearchContent = searchContent.toLowerCase();
+  if (lowerCaseSearchContent.includes(user.userName.toLowerCase())) {
+    return true;
+  }
+
+  if (lowerCaseSearchContent.in)
+}
