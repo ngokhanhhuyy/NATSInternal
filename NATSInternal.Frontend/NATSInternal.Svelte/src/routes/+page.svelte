32059262e930ae20787fb2931248b4c8ea3cfa97
Ngô Khánh Huy <93859822+ngokhanhhuyy@users.noreply.github.com>
2025-11-28 03:01:13 +0900

    feat: initialize SvelteKit project with basic structure and Tailwind CSS integration
    - Added TypeScript support with app.d.ts and tsconfig.json
    - Created main HTML template in app.html
    - Included favicon SVG asset
    - Set up layout and page components with basic state management
    - Integrated Tailwind CSS for styling
    - Added robots.txt for search engine crawling
    - Configured Vite and SvelteKit with necessary plugins and adapters

---
+++ NATSInternal.Frontend/NATSInternal.Svelte/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  let count = $state(0);
+  const array: number[] = $state([]);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<span>{count}</span>
+<button onclick={() => count++} type="button">
+  Increment
+</button>
+<br/>
+<span>List: {array.join(", ")}</span>
+<button type="button" onclick={() => array.push(count)}>
+  Add
+</button>
+
+<style>
+  @reference "tailwindcss";
+  button {
+    @apply bg-white border border-black/10 px-2 py-0.5 rounded-lg shadow-xs cursor-pointer
+      hover:bg-black/2 hover:border-black/15;
+  }
+</style>
