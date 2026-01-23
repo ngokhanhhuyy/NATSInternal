898af36e2375fe96991a8c577641360c0dd80551
nh0zhuybmt <93859822+ngokhanhhuyy@users.noreply.github.com>
2026-01-24 00:32:06 +0900

    Enhance UI components and layouts for better user experience
    - Added properties to DeleteConfirmationModel for action handling.
    - Updated NotFoundErrorPage and ValidationErrorPage layouts for consistency.
    - Improved NavigationBar and TopBar functionality and styling.
    - Adjusted CSS for topbar and panel components to enhance layout and appearance.

--- NATSInternal.Web/Models/Confirmation/DeleteConfirmationModel.cs
+++ NATSInternal.Web/Models/Confirmation/DeleteConfirmationModel.cs
@@ -2,5 +2,9 @@ namespace NATSInternal.Web.Models;
 
 public class DeleteConfirmationModel
 {
-    
+    #region Properties
+    public required string ActionUrl { get; set; }
+    public required string CancelUrl { get; set; }
+    public required string SuccessUrl { get; set; }
+    #endregion
 }
